f65a478a8b54c91ab478f55a0f7babcbc24fb328
Michael <osuperyuu@gmail.com>
2020-09-28 22:03:45 -0300

    update

--- Frotas/fornecedores_editar.php
+++ Frotas/fornecedores_editar.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <?php
+    if (isset($_GET['op'])) {
+        if ($_GET['op'] == 'inc') {
+            echo "<h1>Inclusão de Fornecedores</h1>";
+        } else {
+            echo "<h1>Alteração de Fornecedores</h1>";
+        }
+    }
+    ?>
 </body>
+
 </html>
